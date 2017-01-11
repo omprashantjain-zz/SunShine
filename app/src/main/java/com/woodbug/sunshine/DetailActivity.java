fe489023c020efba8f49f35b136653503f2f6ab8
Prashant Jain <prashant.jain@mubble.in>
2017-01-12 00:32:40 +0530

    SunShine: Refactoring.

--- app/src/main/java/com/woodbug/sunshine/DetailActivity.java
+++ app/src/main/java/com/woodbug/sunshine/DetailActivity.java
@@ -21,28 +21,28 @@ import android.support.v7.app.AppCompatActivity;
 
 public class DetailActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_detail);
 
-        if (savedInstanceState == null) {
-            // Create the detail fragment and add it to the activity
-            // using a fragment transaction.
+    if (savedInstanceState == null) {
+      // Create the detail fragment and add it to the activity
+      // using a fragment transaction.
 
-            Bundle arguments = new Bundle();
-            arguments.putParcelable(com.woodbug.sunshine.DetailFragment.DETAIL_URI, getIntent().getData());
-            arguments.putBoolean(com.woodbug.sunshine.DetailFragment.DETAIL_TRANSITION_ANIMATION, true);
+      Bundle arguments = new Bundle();
+      arguments.putParcelable(com.woodbug.sunshine.DetailFragment.DETAIL_URI, getIntent().getData());
+      arguments.putBoolean(com.woodbug.sunshine.DetailFragment.DETAIL_TRANSITION_ANIMATION, true);
 
-            com.woodbug.sunshine.DetailFragment fragment = new com.woodbug.sunshine.DetailFragment();
-            fragment.setArguments(arguments);
+      com.woodbug.sunshine.DetailFragment fragment = new com.woodbug.sunshine.DetailFragment();
+      fragment.setArguments(arguments);
 
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.weather_detail_container, fragment)
-                    .commit();
+      getSupportFragmentManager().beginTransaction()
+          .add(R.id.weather_detail_container, fragment)
+          .commit();
 
-            // Being here means we are in animation mode
-            supportPostponeEnterTransition();
-        }
+      // Being here means we are in animation mode
+      supportPostponeEnterTransition();
     }
+  }
 }
--- app/src/main/java/com/woodbug/sunshine/widget/TodayWidgetProvider.java
+++ app/src/main/java/com/woodbug/sunshine/widget/TodayWidgetProvider.java
@@ -26,28 +26,28 @@ import com.woodbug.sunshine.sync.SunshineSyncAdapter;
 
 /**
  * Provider for a horizontally expandable widget showing today's weather.
- *
+ * <p>
  * Delegates widget updating to {@link TodayWidgetIntentService} to ensure that
  * data retrieval is done on a background thread
  */
 public class TodayWidgetProvider extends AppWidgetProvider {
 
-    @Override
-    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        context.startService(new Intent(context, TodayWidgetIntentService.class));
-    }
+  @Override
+  public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+    context.startService(new Intent(context, TodayWidgetIntentService.class));
+  }
 
-    @Override
-    public void onAppWidgetOptionsChanged(Context context, AppWidgetManager appWidgetManager,
-                                          int appWidgetId, Bundle newOptions) {
-        context.startService(new Intent(context, TodayWidgetIntentService.class));
-    }
+  @Override
+  public void onAppWidgetOptionsChanged(Context context, AppWidgetManager appWidgetManager,
+                                        int appWidgetId, Bundle newOptions) {
+    context.startService(new Intent(context, TodayWidgetIntentService.class));
+  }
 
-    @Override
-    public void onReceive(@NonNull Context context, @NonNull Intent intent) {
-        super.onReceive(context, intent);
-        if (SunshineSyncAdapter.ACTION_DATA_UPDATED.equals(intent.getAction())) {
-            context.startService(new Intent(context, TodayWidgetIntentService.class));
-        }
+  @Override
+  public void onReceive(@NonNull Context context, @NonNull Intent intent) {
+    super.onReceive(context, intent);
+    if (SunshineSyncAdapter.ACTION_DATA_UPDATED.equals(intent.getAction())) {
+      context.startService(new Intent(context, TodayWidgetIntentService.class));
     }
+  }
 }
